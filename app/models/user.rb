358182ed417f5032b001a29fb06691ec0f615716
Jean Mendonça <pierrybm@gmail.com>
2024-02-24 15:10:11 -0300

    Revert "ADD KIND_OF TO USERS"
    This reverts commit 6b8ff03340969d110af151d0686f493abc0e4d43.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  devise :database_authenticatable, :registerable, :recoverable,
-         :rememberable, :validatable, :trackable, :lockable
-
-  enum kind_of: { pacient: 0, psychologist: 1, admin: 2 }
+  devise :database_authenticatable,
+         :registerable, :recoverable, :rememberable, :validatable, :trackable, :lockable
 end
--- db/migrate/20240224180059_add_kind_of_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddKindOfToUsers < ActiveRecord::Migration[7.1]
-  def change
-    add_column :users, :kind_of, :integer, null: false, default: 0
-  end
-end
